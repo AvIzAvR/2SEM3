b193ab8d8ab3cda0129ee630c0aefad45c64c472
AvIzAvR <108844960+AvIzAvR@users.noreply.github.com>
2023-05-16 23:07:53 +0300

    Delete defines.h

--- defines.h
+++
@@ -1,8 +0,0 @@
-#include "struct.h"
-
-BMPFile* readBMP(const char* path);
-void writeBMP(const char* path, BMPFile* bmp);
-void invert_colors(BMPFile *image);
-void black_white_colors(BMPFile *image);
-void median_filter(BMPFile *image, int window_size);
-void gamma_correction(BMPFile *image, double gamma);
